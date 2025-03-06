422611bdc02d0a386f040d181bb9489d884631bc
Ankita Kumari <ankita00iv@gmail.com>
2025-03-07 00:31:46 +0530

    initial commit

---
+++ app/src/main/java/com/training/app/DataModel/Items.java
@@ -0,0 +1,14 @@
+package com.training.app.DataModel;
+
+import android.media.RouteListingPreference;
+public class Items {
+
+        private String text;
+        public Items(String text){this.text=text;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+}
